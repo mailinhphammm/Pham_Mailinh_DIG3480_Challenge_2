217f38bc295985ed11cc694362f7164914c0cf6d
mailinhphammm <44384480+mailinhphammm@users.noreply.github.com>
2019-10-27 23:17:24 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+    private GameObject Player;
+
+    public float speed;
+    public float jumpForce;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
+    public AudioSource Win;
+    public bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+
+        lives = 3;
+        SetLivesText();
+        loseText.text = "";
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+        if (moveHorizontal < 0 && facingRight) Flip();
+        if (moveHorizontal > 0 && !facingRight) Flip();
+
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+       if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 8)
+        {
+            winText.text = "You Win!";
+            GetComponent<AudioSource>().Play();
+        }
+
+        if (count == 4)
+        {
+            Player.transform.position = new Vector4(-47, -2, 0);
+            lives = 3;
+            SetLivesText();
+        }
+    }
+
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+        if (lives == 0)
+        {
+            loseText.text = "You Lose!";
+            Destroy(Player);
+            Application.Quit();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+}
